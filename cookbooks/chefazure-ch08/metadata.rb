43510896f938dcf4ec1b912907ccbcc0f7a39add
Danny Crone <dan_crone@nfocus.co.uk>
2016-07-05 18:41:46 +0100

    adding dev server

--- cookbooks/chefazure-ch08/metadata.rb
+++ cookbooks/chefazure-ch08/metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'dan.crone@nfocus.co.uk'
 license 'all_rights'
 description 'Installs/Configures chefazure-ch08'
 long_description 'Installs/Configures chefazure-ch08'
-version '0.1.6'
+version '0.1.7'
 
 depends 'jenkins'
