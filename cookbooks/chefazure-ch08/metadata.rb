3b970ba56b86b2f1e8c76225faa6cb72d2c48dc9
Danny Crone <dan_crone@nfocus.co.uk>
2016-07-05 11:41:39 +0100

    Initial add

---
+++ cookbooks/chefazure-ch08/metadata.rb
@@ -0,0 +1,9 @@
+name 'chefazure-ch08'
+maintainer 'Danny Crone'
+maintainer_email 'dan.crone@nfocus.co.uk'
+license 'all_rights'
+description 'Installs/Configures chefazure-ch08'
+long_description 'Installs/Configures chefazure-ch08'
+version '0.1.4'
+
+depends 'jenkins'
