89063d2abc05762deafd9704b39080c05dd10ef9
Danny Crone <dan_crone@nfocus.co.uk>
2016-07-07 17:11:17 +0100

    Updated to chec

--- cookbooks/chefazure-ch08/recipes/default.rb
+++ cookbooks/chefazure-ch08/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: chefazure-ch08
 # Recipe:: default
 #
-# Copyright (c) 2016 Danny Crone, All Rights Reserved.
+# Copyright (c) 2016 Danny Crone inc, All Rights Reserved.
 
 include_recipe 'chefazure-ch08::provision_dev'
 include_recipe 'chefazure-ch08::provision_test'
